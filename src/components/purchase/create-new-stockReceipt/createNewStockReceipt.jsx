d649d42cf11fbefc913e02dcf1aa1947ea65e631
kamalrajms <kamalrajindtn@gmail.com>
2025-07-08 18:33:01 +0530

    stock receipt  his,comment,attachment

--- src/components/purchase/create-new-stockReceipt/createNewStockReceipt.jsx
+++ src/components/purchase/create-new-stockReceipt/createNewStockReceipt.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import "./createNewStockReceipt.css";
 import { useNavigate } from "react-router-dom";
+import StockComment from "./stockComment";
+import StockHistory from "./stockHistory";
+import StockAttachment from "./stockAttachment";
 
 export default function () {
   const prevpg = useNavigate();
   const [stockReceiptStatus, setStockReceiptstatus] = useState("");
+  const [detail, setDetail] = useState({
+    comment: true,
+    history: false,
+    attachment: false,
+  });
   return (
     <>
       <div className="cerateNewStock-container">
@@ -83,14 +91,158 @@ export default function () {
         </div>
         <div className="cerateNewStock-input-container">
           <div className="cerateNewStock-input-box">
-            <label htmlFor="received_date">Received Date*</label>
+            <label htmlFor="received_date">
+              Received Date<sup>*</sup>
+            </label>
             <input id="received_date" type="date" required />
           </div>
           <div className="cerateNewStock-input-box">
-            <label htmlFor="">
+            <label htmlFor="supplier_name">
               Supplier Name<sup>*</sup>
             </label>
-            <input type="text" id="" required />
+            <input
+              type="text"
+              id="supplier_name"
+              placeholder="Enter Supplier Name"
+              required
+            />
+          </div>
+        </div>
+        <div className="cerateNewStock-input-container">
+          <div className="cerateNewStock-input-box">
+            <label htmlFor="supplier_dn_no">Supplier DN No.</label>
+            <input
+              id="supplier_dn_no"
+              type="text"
+              placeholder="Enter Supplier DN No."
+            />
+          </div>
+          <div className="cerateNewStock-input-box">
+            <label htmlFor="supplier_invoice_no">Supplier Invoice No.</label>
+            <input
+              type="text"
+              id="supplier_invoice_no"
+              placeholder="Enter Supplier Invoice No."
+            />
+          </div>
+        </div>
+        <div className="cerateNewStock-input-container">
+          <div className="cerateNewStock-input-box">
+            <label htmlFor="received_by">Received By</label>
+            <select id="received_by">
+              <option value="">Select Referance</option>
+              <option value="one">One</option>
+            </select>
+          </div>
+          <div className="cerateNewStock-input-box">
+            <label htmlFor="qc_done_by">QC Done By</label>
+            <select id="qc_done_by">
+              <option value="">Select Referance</option>
+              <option value="one">One</option>
+            </select>
+          </div>
+        </div>
+        <nav className="cerateNewStock-subtit">
+          Line Items<sup>*</sup>
+        </nav>
+        <div className="cerateNewStock-table-container">
+          <table>
+            <thead className="cerateNewStock-table-head">
+              <tr>
+                <th>#</th>
+                <th>
+                  <pre>Product Name</pre>
+                </th>
+                <th>
+                  <pre>Product ID</pre>
+                </th>
+                <th>UMO</th>
+                <th>
+                  <pre>Qty Ordered</pre>
+                </th>
+                <th>
+                  <pre>Qty Received</pre>
+                </th>
+                <th>
+                  <pre>Accepted Qty</pre>
+                </th>
+                <th>
+                  <pre>Rejected Qty</pre>
+                </th>
+                <th>
+                  <pre>Qty Returned</pre>
+                </th>
+                <th>
+                  <pre>Stock Dim.</pre>
+                </th>
+                <th> Warehouse</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody className="cerateNewStock-table-body">
+              <tr></tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="cerateNewStock-hub-container">
+          <div className="cerateNewStock-hub-head">
+            <p
+              className={
+                detail.comment
+                  ? "cerateNewStock-hub-head-bg-black"
+                  : "cerateNewStock-hub-head-tit"
+              }
+              onClick={() => {
+                setDetail({
+                  history: false,
+                  attachment: false,
+                  comment: true,
+                });
+              }}
+            >
+              Comments
+            </p>
+            <p
+              className={
+                detail.history
+                  ? "cerateNewStock-hub-head-bg-black"
+                  : "cerateNewStock-hub-head-tit"
+              }
+              onClick={() => {
+                setDetail({
+                  history: true,
+                  attachment: false,
+                  comment: false,
+                });
+              }}
+            >
+              History
+            </p>
+            <p
+              className={
+                detail.attachment
+                  ? "cerateNewStock-hub-head-bg-black"
+                  : "cerateNewStock-hub-head-tit"
+              }
+              onClick={() => {
+                setDetail({
+                  history: false,
+                  attachment: true,
+                  comment: false,
+                });
+              }}
+            >
+              Attachments
+            </p>
+          </div>
+          <div className="cerateNewStock-hub-body">
+            {detail.comment && <StockComment />}
+            {detail.history && <StockHistory />}
+            {detail.attachment && (
+              <StockAttachment
+              // inputDisable={purchaseBtn.buttonAcs}
+              />
+            )}
           </div>
         </div>
       </div>
